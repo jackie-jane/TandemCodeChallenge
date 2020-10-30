612c549e4433b5522c4793d7664878e5d177cd92
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-10-30 16:57:19 -0400

    began work on the functionality of the application. This includes the Quiz component, which will render questions. Before questions are rendered we will first render a begin button, which begins the quiz

---
+++ src/components/beginButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BeginButton(props) {
+  return (
+    <button>
+      Begin The Quiz!
+    </button>
+  );
+}
+
+export default BeginButton;
---
+++ src/components/quiz.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import BeginButton from './beginButton'
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inProgress: 0
+    }
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <>
+
+      </>
+    );
+  }
+}
+export default Quiz;
